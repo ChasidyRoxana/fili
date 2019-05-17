c64c26bf7d417986277807efe606f8d8394f7997
Chasidy Roxana <croxana@at-h4.21-school.ru>
2019-05-17 14:09:06 +0300

    for T. v.2

--- print.c
+++ print.c
@@ -6,7 +6,7 @@
 /*   By: croxana <croxana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 13:35:40 by croxana           #+#    #+#             */
-/*   Updated: 2019/05/16 14:41:08 by croxana          ###   ########.fr       */
+/*   Updated: 2019/05/17 13:35:25 by croxana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,24 @@ void	ft_print_map(t_tet *head)
 	int c;
 	int k;
 
-	i = 0;
-	c = 15;
+	//printf("print\n");
 	while (head != NULL)
 	{
-		while (i < 4)
+		i = -1;
+		while (++i < 4)
 		{
+			c = 15;
 			while (c > 11)
 			{
-				k = head->elem[i] >> c;
+				k = (head->elem)[i] >> c--;
 				if (k & 1)
 					write(1, &(head->symbol), 1);
 				else
-					write(1, ".", 1);				
-				c--;
+					write(1, ".", 1);
 			}
-			i++;
-			c = 15;
+			write(1, "\n", 1);
 		}
 		head = head->next;
+		write(1, "\n", 1);
 	}
 }
--- read.c
+++ read.c
@@ -6,13 +6,13 @@
 /*   By: croxana <croxana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 11:55:33 by croxana           #+#    #+#             */
-/*   Updated: 2019/05/16 14:55:20 by croxana          ###   ########.fr       */
+/*   Updated: 2019/05/17 13:54:30 by croxana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-t_tet	*ft_new_node(t_tet *head)
+static t_tet	*ft_new_node(t_tet *head)
 {
 	t_tet	*new;
 	char	sym;
@@ -25,7 +25,7 @@ t_tet	*ft_new_node(t_tet *head)
 		new = (t_tet*)malloc(sizeof(t_tet));
 	else
 	{
-		while (new->next != NULL)
+		while (new->next)
 		{
 			new = new->next;
 			sym++;
@@ -43,58 +43,64 @@ t_tet	*ft_new_node(t_tet *head)
 	return(new);
 }
 
-int	ft_read(char *file_name)
+int				ft_read(char *file_name)
 {
 	int		fd;
-	char	**line;
+	char	*line;
+	t_tet	*head;
+	t_tet	*tet;
+	int		gnl;
+	int		new;//
+
 	int		i;
 	int		j;
-	t_tet	*head;
 	int		n;
-	t_tet	*tet;
-	int		new;
-	int 	gnl;
 
 	fd = open(file_name, O_RDONLY);
-	line = malloc(1);
 	head = NULL;
 	new = 0;
 	gnl = 0;
-	while (get_next_line(fd, line) == 1)
+	while (get_next_line(fd, &line) == 1)
 	{
-		printf("while\n");
 		if (gnl == new)
 		{
 			j = 0;
-			printf("tet\n");
-			tet = ft_new_node(head);
+			if (!head)
+			{
+				head = ft_new_node(head);
+				tet = head;
+			}
+			else
+			{
+				while (tet->next)
+					tet = tet->next;
+				tet->next = ft_new_node(head);
+				tet = tet->next;
+			}
 			new += (new == 0 ? 4 : 5);
 			if (gnl != 0)
 			{
-				if (get_next_line(fd, line) != 1){printf("1 if in while\n");
-					return (0);}
+				if (get_next_line(fd, &line) != 1)
+					return (0);
 				gnl++;
 			}
 		}
 		i = 0;
 		n = 15;
-		while(*line[i] != '\0')
+		while (line[i] != '\0')
 		{
-			printf("line[i]\n");
-			if (*line[i] != '#' && *line[i] != '.'){printf("1 if while while\n");
-				return (0);}
-			if (*line[i] == '#')
+			if (line[i] != '#' && line[i] != '.')
+				return (0);
+			if (line[i++] == '#')
 			{
-				printf("if == #\n");
 				if (tet->f_sym == -1 || i < tet->f_sym)
 					tet->f_sym = i;
 				(tet->elem)[j] = (tet->elem)[j] | (1 << n);
 			}
-			i++;
 			n--;
 		}
-		if (i != 4){printf("i: %d\n", i);
-			return (0);}
+		if (i != 4)
+			return (0);
 		gnl++;
 		j += (j == 0 && (tet->elem)[j] == 0) ? 0 : 1;
 	}
